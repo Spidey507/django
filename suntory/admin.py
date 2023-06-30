bdd0a3c97137af5ca718816b602979a9a685523f
Spidey507 <miguelalveo@gmail.com>
2023-06-29 21:32:21 -0500

    added the review model and made it so that every product displays its reviews respectively

--- suntory/admin.py
+++ suntory/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Review
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('title', 'product')
--- suntory/views.py
+++ suntory/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product, Distillery
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product, Distillery, Review
 
 def product_list(request):
     products = Product.objects.all()
@@ -11,4 +11,22 @@ def distillery_info(request):
 
 def product_details(request, id):
     product = get_object_or_404(Product, id=id)
-    return render(request, 'suntory/product_details.html', {'product': product})
+    reviews = product.reviews.all()
+    return render(request, 'suntory/product_details.html', {'product': product, 'reviews': reviews})
+
+def review_form(request, id):
+    product = Product.objects.get(id=id)
+
+    if request.method == 'POST':
+        # Process the form data and save the review
+        title = request.POST.get('title')
+        body = request.POST.get('body')
+
+        # Create a new Review object associated with the product and save it to the database
+        review = Review(title=title, body=body, product=product)
+        review.save()
+
+        # Redirect the user to the product_details page
+        return redirect('suntory:product_details', id=id)
+
+    return render(request, 'suntory/review_form.html', {'product': product})
